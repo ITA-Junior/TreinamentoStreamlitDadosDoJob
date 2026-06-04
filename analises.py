4029ddbc6d79d1c50dbebe0bb89d37be694b3446
lucasresend3 <lucasresende20053@gmail.com>
2026-06-04 10:19:05 -0300

    As 10 questões estão em analises.py

--- analises.py
+++ analises.py
@@ -4,104 +4,104 @@ import pandas as pd
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
-#como instalar elas
 
 def q1(df_orders):
+    df_office = df_orders[df_orders["Category"] == "Office Supplies"]
 
-    df_office = df_orders[
-        df_orders["Category"] == "Office Supplies"
-    ].copy()
+    resultado = (df_office.groupby("City")["Total Sales"]
+    .sum()
+    .sort_values(ascending = False))
 
-    df_office["Valor_Venda"] = (
-        df_office["Sales"] * df_office["Quantity"]
-    )
-
-    resultado = (
-        df_office.groupby("City")["Valor_Venda"]
-        .sum()
-        .sort_values(ascending=False)
-    )
-
-    return {
+    return{
         "cidade": resultado.idxmax(),
         "valor": resultado.max()
     }
-   
+
 
 def q2(df_orders):
+
     df = df_orders.copy()
 
     df["Order Date"] = pd.to_datetime(df["Order Date"])
 
-    df["Valor_Venda"] = (
-        df["Sales"] * df["Quantity"]
-    )
+    return(
 
-    return (
-        df.groupby("Order Date")["Valor_Venda"]
+        df.groupby("Order Date")["Total Sales"]
         .sum()
         .reset_index()
         .sort_values("Order Date")
     )
-
+   
 
 def q3(df_orders):
 
-    df = df_orders.copy()
-
-    df["Valor_Venda"] = (
-        df["Sales"] * df["Quantity"]
-    )
-
-    return (
-        df.groupby("State")["Valor_Venda"]
+    return(
+        df_orders.groupby("State")["Total Sales"]
         .sum()
         .reset_index()
-        .sort_values("Valor_Venda", ascending=False)
-        .reset_index(drop=True)
+        .sort_values("Total Sales", ascending = False)
+        .reset_index(drop = True)
     )
 
 
 def q4(df_orders):
 
-    df = df_orders.copy()
-
-    df["Valor_Venda"] = (
-        df["Sales"] * df["Quantity"]
-    )
-
-    return (
-        df.groupby("City")["Valor_Venda"]
+    return(
+        df_orders.groupby("City")["Total Sales"]
         .sum()
         .reset_index()
-        .sort_values("Valor_Venda", ascending=False)
+        .sort_values("Total Sales", ascending = False)
         .head(10)
-        .reset_index(drop=True)
+        .reset_index(drop = True)
+    )
+
+
+def q5(df_orders):
+
+    return(
+        df_orders.groupby("Segment")["Total Sales"]
+        .sum()
+        .reset_index()
+        .sort_values("Total Sales", ascending = False)
+    )
+
+
+def q6(df_orders):
+
+    df_copia = df_orders.copy()
+
+    df_copia["Order Date"] = pd.to_datetime(df_copia["Order Date"])
+    df_copia["Year"] = df_copia["Order Date"].dt.year
+
+    return(
+        df_copia.groupby(["Segment", "Year" ])["Total Sales"]
+        .sum()
+        .reset_index()
+        .sort_values(["Year", "Segment"])
     )
 
 
 def q7(df_orders):
 
-    valor_venda = (
-        df_orders["Sales"] * df_orders["Quantity"]
-    )
-
-    return int((valor_venda > 1000).sum())
+    return{ 
+        
+        "Qtd com 15% de desconto": (df_orders["Total Sales"] > 1000).sum(),
+        "Qtd com 10% de desconto": (df_orders["Total Sales"] <= 1000).sum()
+    }
 
 
 def q8(df_orders):
+    
+    media_antes = df_orders["Total Sales"].mean()
 
-    valor_venda = (
-        df_orders["Sales"] * df_orders["Quantity"]
-    )
-
-    media_antes = valor_venda.mean()
+    # X = A.where(condicao, B)
+    # se a condicao for cumprida, X = A, se nao, X = B
 
     vendas_com_desconto = (
-        valor_venda * 0.85
+        df_orders["Total Sales"] * 0.85
     ).where(
-        valor_venda > 1000,
-        valor_venda * 0.90
+        df_orders["Total Sales"] > 1000,
+        df_orders["Total Sales"] * 0.90
     )
 
     media_depois = vendas_com_desconto.mean()
@@ -109,21 +109,36 @@ def q8(df_orders):
     return {
         "media_antes": media_antes,
         "media_depois": media_depois
-    }
+
+}
+
+
+def q9(df_orders):
+
+    df_copia = df_orders.copy()
+    df_copia["Order Date"] = pd.to_datetime(df_copia["Order Date"])
+
+    df_copia["Year"] = df_copia["Order Date"].dt.year
+    df_copia["Month"] = df_copia["Order Date"].dt.month
+
+    return(
+        df_copia.groupby(["Segment", "Year", "Month"])["Total Sales"]
+        .mean()
+        .reset_index()
+        .sort_values(["Year", "Month", "Segment"])
+        .reset_index(drop = True)
+    )
+
 
 def q10(df_orders):
 
-    df = df_orders.copy()
-
-    df["Valor_Venda"] = (
-        df["Sales"] * df["Quantity"]
-    )
-
-    return (
-        df.groupby(["Category", "Sub-Category"])["Valor_Venda"]
+    return(
+        df_orders.groupby(["Category", "Sub-Category"])["Total Sales"]
         .sum()
         .reset_index()
-        .sort_values("Valor_Venda", ascending=False)
+        .sort_values("Total Sales", ascending = False)
         .head(12)
-        .reset_index(drop=True)
+        .reset_index(drop = True)
     )
+
+
