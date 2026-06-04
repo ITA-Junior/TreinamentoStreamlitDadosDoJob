57be4f5f7b9ed9a9232c03ccc70e6fce4d33fd22
Lucas Resende <Lucasresende20053@gmail.com>
2026-06-04 10:41:29 -0300

    Inicio do projeto

--- streamlip_app/main.py
+++ streamlip_app/main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import analises
-import data
+import dados
 
 st.header("DADOS DO JOOOOBBBBB 🎲🎲🍆🥛")
 
-st.dataframe(data.df)
+st.dataframe(dados.df)
